10809a238bb3c19e2bfe70b75eb385d68865d733
Ben Watts <thebenwatts@gmail.com>
2013-12-22 16:02:48 -0500

    set up grunt to watch/compile sass + coffee files

---
+++ Gruntfile.js
@@ -0,0 +1,64 @@
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+
+    sass: {
+      dist: {
+        options: {
+          style: 'expanded',
+          debugInfo: true,
+          lineNumbers: false
+        },
+        files: {
+          'css/build/style.css': 'css/style.scss'
+        }
+      }
+    },
+
+    coffee: {
+      compileJoinedWithMaps: {
+        options: {
+          sourceMap: true,
+          join: true
+        },
+        files: {
+          'js/build/app.js': 'js/*.coffee'
+        }
+      }
+    },
+
+    watch: {
+      options: {
+        livereload: true
+      },
+      css: {
+        files: ['css/*.scss'],
+        tasks: ['sass'],
+        options: {
+          spawn: false
+        }
+      },
+      js: {
+        files: ['js/*.coffee'],
+        tasks: ['coffee'],
+        options: {
+          spawn: false
+        }
+      },
+      gruntfile: {
+        files: ['Gruntfile.js']
+      }
+    },
+
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-coffee');
+  grunt.loadNpmTasks('grunt-contrib-livereload');
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  //grunt.loadNpmTasks('grunt-contrib-uglify');
+
+  grunt.registerTask('default', ['watch', 'sass', 'coffee', 'livereload']);
+
+};
--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # A readme
+
+Setup:
+
+1) External dependencies: ruby, sass, node.
+2) Grunt CLI: npm install -g grunt-cli
